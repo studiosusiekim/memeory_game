1c8726fb64aacf814657e89e689fe5513203a732
Susie Kim <hello@susiekim.de>
2020-01-26 22:04:59 +0100

    game is on

--- scripts.js
+++ scripts.js
@@ -1,10 +1,12 @@
 const cards = document.querySelectorAll('.memory-card');
 
 let hasFlippedCard = false;
+let lockBoard = false;
 let firstCard, secondCard;
 
 
 function flipCard() {
+    if(lockBoard) return; // if it's not a match the board is locked
     this.classList.toggle('flip');
     if(!hasFlippedCard) {
         // first turn
@@ -14,20 +16,69 @@ function flipCard() {
         // second turn 
         hasFlippedCard = false;
         secondCard = this;
-    
-        //do cards match?
-            if(firstCard.dataset.framework === secondCard.dataset.framework) {
-            // matching?
-            firstCard.removeEventListener('click', flipCard);
-            secondCard.removeEventListener('click', flipCard);
-            } else {
-            // not matching!
-            setTimeout(() => {
-                firstCard.classList.remove('flip');
-                secondCard.classList.remove('flip');
-            }, 1500);
-            }   
-        }
-    }   
 
-cards.forEach(card => card.addEventListener('click', flipCard));
+        checkForMatch();
+    }
+}
+    
+//         //do cards match?
+//             if(firstCard.dataset.framework === secondCard.dataset.framework) {
+//             // matching?
+//             firstCard.removeEventListener('click', flipCard);
+//             secondCard.removeEventListener('click', flipCard);
+//             } else {
+//             // not matching!
+//             setTimeout(() => {
+//                 firstCard.classList.remove('flip');
+//                 secondCard.classList.remove('flip');
+//             }, 1500);
+//             }   
+//         }
+//     }   
+
+
+
+//do cards match?
+function checkForMatch() {
+    let isMatch = firstCard.dataset.framework === secondCard.dataset.framework 
+
+    isMatch ? disableCards() : unflipCards();
+
+    // if(firstCard.dataset.framework === secondCard.dataset.framework) {
+    //     disableCards();
+    // } else {
+    //     unflipCards();
+    // }   
+}
+    
+// matching?
+function disableCards() {
+    firstCard.removeEventListener('click', flipCard);
+    secondCard.removeEventListener('click', flipCard);
+}
+
+// not matching!
+function unflipCards() {
+    lockBoard = true;
+  
+    setTimeout(() => {
+      firstCard.classList.remove('flip');
+      secondCard.classList.remove('flip');
+  
+      resetBoard();
+    }, 1500);
+  }
+  
+function resetBoard() {
+[hasFlippedCard, lockBoard] = [false, false];
+[firstCard, secondCard] = [null, null];
+}
+
+(function shuffle() {
+    cards.forEach(card => {
+        let randomPos = Math.floor(Math.random() * 12);
+        card.style.order = randomPos;
+    });
+    })();
+    
+    cards.forEach(card => card.addEventListener('click', flipCard));
